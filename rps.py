adf9ec22a530381ea1d832f0c4f999da3cba6f48
Brandon Ward <brandonwardpm@gmail.com>
2022-07-01 18:43:43 -0400

    Assigned rps array to var 'moves' in Player class

--- rps.py
+++ rps.py
@@ -3,15 +3,12 @@ import random
 """This program plays a game of Rock, Paper, Scissors between two Players,
 and reports both Player's scores each round."""
 
-moves = ['rock', 'paper', 'scissors']
-
 """The Player class is the parent class for all of the Players
 in this game"""
 
 
 class Player:
-    def move(self):
-        return 'rock'
+    moves = ['rock', 'paper', 'scissors']
 
     def learn(self, my_move, their_move):
         pass
